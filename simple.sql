8d4c6caf015925126003cd8cdff44a6f9fdd4306
KrishnaF610 <matematikaonline77@gmail.com>
2024-08-08 13:43:23 +0700

    belajar database dasar

--- simple.sql
+++ simple.sql
@@ -35,3 +35,48 @@ UPDATE students SET grade = 'B+' WHERE grade = 'B';
 # Sama dengan update jika kita menggunakan kondisi atau where maka kita akan menghapus semua data
 # Jika kita ingin menghapus data yang kita pilih maka gunakan where
 DELETE FROM students where id = 2;
+
+
+
+CREATE TABLE teachers (
+    id bigint PRIMARY Key AUTO_INCREMENT,
+    name varchar(191) not null
+);
+
+# column teacher_id pada table classes merupakan foreign key atau penghubung antara table,
+# teachers dan classes dimana 1 kelas itu dimiliki oleh 1 guru (One to One) atau
+# 1 guru bisa punya banyak kelas (One to Many)
+# Cara mendefinisikan forign key cukup dengan
+# CONSTRAINT FOREIGN KEY (column) REFERENCE <table> (column) (option)
+CREATE TABLE classes (
+    id bigint PRIMARY KEY AUTO_INCREMENT,
+    teacher_id bigint not null,
+    NAME varchar(191) not null
+
+    CONSTRAINT
+        FOREIGN KEY (teacher_id)
+        REFERENCES teachers (id) ON DELETE CASCADE
+);
+
+INSERT INTO teacher_id (name) VALUE ('Wira');
+
+# Ketika kita mau menambahkan data relational kita perlu tahu dulu id dari parent table
+# dalam kasus ini parent table adalah teacher, dimana kita mau menambahkan kelas
+# terhadap teacher dengan nama Wira, dimana Wira memiliki 2 kelas yaitu sistem Informasi dan
+# Teknik Informasi
+INSERT INTO classes (teacher_id, name) VALUES
+(1, 'Sistem Informasi'),
+(1, 'Teknik Informasi');
+
+# dalam proses pengambilan data dari 2 atau lebih table kita perlu melakukan pemanggilan
+# column dengan nama <table>.<column>. Jika data yang ingin kita tampilkan ganti nama,
+# column gunakan AS sebagai alias atau pengganti sementara dari nama column
+# Dalam kasus ini kita mencoba mengganti data dari table classes dan ingin menambahi data
+# Guru berdasarkan column teacher_id yang ada di table classes dengan menggunakan INNER JOIN
+# dimana INNER JOIN membutuhkan foreign key sebagai kunci asing atau sebagai izin untuk
+# mengakses table yang kita mau, dalam hal ini table teachers yang akan kita ambil datanya
+SELECT 
+    classes.name as nama_kelas, 
+    teachers.name as nama_guru
+FROM classes
+INNER JOIN teachers on classes.teacher_id = teachers.id
